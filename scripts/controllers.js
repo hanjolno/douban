bdd72fd9916a9477a8df8ba47388a367cd02833e
nanjolno <385082206@qq.com>
2017-06-10 16:53:28 +0800

    作者完成

--- scripts/controllers.js
+++ scripts/controllers.js
@@ -11,39 +11,39 @@ angular.module("controllers",[])
 }])
 .controller("todayCon",["$scope","$filter","$http","$rootScope",function($scope,$filter,$http,$rootScope){
 	$scope.today=$filter("date")(new Date(),"yyyy-MM-dd");
-	$rootScope.loaded=true;
+	$rootScope.loaded=false;
 	$http({
 		url:"./api/today.php",
 		params:{"today":$scope.today}
 	}).then(function(data){
 		$scope.datas=data.data.posts;
 		// console.log($scope.datas)
-		$rootScope.loaded=false;
+		$rootScope.loaded=true;
 	})
 	$rootScope.title="今日一刻";
 }])
 .controller("olderCon",["$scope","$filter","$http","$rootScope",function($scope,$filter,$http,$rootScope){
 	$scope.older=$filter("date")(new Date(new Date-24*60*60*1000),"yyyy-MM-dd");
-	$rootScope.loaded=true;
+	$rootScope.loaded=false;
 	$http({
 		url:"./api/older.php",
 		params:{"older":$scope.older}
 	}).then(function(data){
 		$scope.datas=data.data.posts;
 		// console.log($scope.datas)
-		$rootScope.loaded=false;
+		$rootScope.loaded=true;
 	})
 	$rootScope.title="往期内容";
 }])
 .controller("authorCon",["$scope","$http","$rootScope",function($scope,$http,$rootScope){
-	$rootScope.loaded=true;
+	$rootScope.loaded=false;
 	$http({
 		url:"./api/author.php"
 	}).then(function(data){
 		$scope.greats=data.data[0].authors;
 		$scope.alls=data.data[1].authors;
 		// console.log($scope.greats);
-		$rootScope.loaded=false;
+		$rootScope.loaded=true;
 	})
 	$rootScope.title="热门作者";
 }])
